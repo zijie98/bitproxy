4bba1efe97f6dd1d79d80673da23b1d4103c894b
moli <molisoft@qq.com>
2017-06-12 23:17:15 +0800

    Add pidfile

--- rkproxy.go
+++ rkproxy.go
@@ -15,7 +15,10 @@ import (
 	"runtime"
 )
 
-var config_path = "config.json"
+var (
+	config_path = "config.json"
+	pid_path    = "rkproxy.pid"
+)
 
 var man *manager.Manager
 var log *logger.Logger = logger.NewLogger("Main")
@@ -35,13 +38,28 @@ func listen_signal() {
 	}()
 }
 
+func init_flag() {
+	flag.StringVar(&config_path, "c", "config.json", "配置文件")
+	flag.StringVar(&pid_path, "p", "rkproxy.pid", "进程id路径")
+	flag.Parse()
+}
+
+func init_pid() {
+	file, err := os.OpenFile(pid_path, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("写pid文件错误：", err)
+		return
+	}
+	defer file.Close()
+	file.Write([]byte(fmt.Sprintf("%d", os.Getpid())))
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
+	init_flag()
+	init_pid()
 	listen_signal()
-
-	flag.StringVar(&config_path, "c", "config.json", "配置文件")
-	flag.Parse()
 
 	man = manager.New(config_path)
 

497fc8be71eb2fef58750a51759c10026c8c24b1
moli <molisoft@qq.com>
2017-07-14 11:15:05 +0800

    fixed traffic stats

--- libs/traffic_stats.go
+++ libs/traffic_stats.go
@@ -14,6 +14,7 @@ type Stats struct {
 }
 
 var trafficStats = make(chan *Stats, 512)
+var deleteTrafficStats = make(chan *Stats, 128)
 var RedisPool *redis.Pool
 
 func InitRedis(host string, port uint) {
@@ -26,13 +27,11 @@ func InitRedis(host string, port uint) {
 }
 
 func AddTrafficStats(port uint, traffic int) {
-	trafficStats <- &Stats{port, traffic}
+	trafficStats <- &Stats{Port: port, Traffic: traffic}
 }
 
-func DeleteTrafficStats(port uint) error {
-	key := statsKey(port)
-	conn := RedisPool.Get()
-	return conn.Send("DEL", key)
+func DeleteTrafficStats(port uint) {
+	deleteTrafficStats <- &Stats{Port: port}
 }
 
 func StartStats() {
@@ -42,12 +41,25 @@ func StartStats() {
 			statsToRedis(s)
 		}
 	}()
+
+	go func() {
+		select {
+		case s := <-deleteTrafficStats:
+			deleteFromTheRedis(s)
+		}
+	}()
 }
 
 func statsToRedis(s *Stats) error {
 	conn := RedisPool.Get()
 	key := statsKey(s.Port)
 	return conn.Send("INCRBY", key, s.Traffic)
+}
+
+func deleteFromTheRedis(s *Stats) error {
+	key := statsKey(s.Port)
+	conn := RedisPool.Get()
+	return conn.Send("DEL", key)
 }
 
 func statsKey(port uint) string {

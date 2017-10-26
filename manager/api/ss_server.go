650d79301ddace7716b4c57cf98535104263183f
moli <molisoft@qq.com>
2017-10-27 01:01:23 +0800

    [api] 在创建前暂停一下ss

--- manager/api/ss_server.go
+++ manager/api/ss_server.go
@@ -16,6 +16,7 @@ func CreateSsServer(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
+	stopSs(server_config)
 	proxy := manager.Man.CreateProxy(server_config, true)
 	e := make(chan error)
 	go func() {
